bc5570eb719924128af643d3cb32572122951838
Olav Stetter <olav.stetter@googlemail.com>
2024-09-04 21:48:50 +0200

    Working scoring.

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,31 @@ fn main() {
     println!("{}", float_axis_labels(min, max, 60));
 }
 
-const MAX_LABELS:usize = 100;
+const MAX_LABELS: usize = 100;
 struct FloatLabelSet {
     nr_labels: i64,
-    labels: [f64;MAX_LABELS]
+    labels: [f64; MAX_LABELS],
 }
 
-const MAX_SKIP_AMOUNT:i32 = 9;
-const Q_VALUES: [f64;6] = [1.0, 5.0, 2.0, 2.5, 4.0, 3.0];
+const MAX_SKIP_AMOUNT: i32 = 9;
+const Q_VALUES: [f64; 6] = [1.0, 5.0, 2.0, 2.5, 4.0, 3.0];
 
-fn float_axis_labels(x_min: f64, x_max: f64, _available_space: i64) -> String {
+fn float_axis_labels(x_min: f64, x_max: f64, available_space: i64) -> String {
     let data_range = x_max - x_min;
     let base_exponent = data_range.log10() as i64;
     println!("DEBUG: base_exponent = {base_exponent}");
+    let preferred_nr_labels = compute_preferred_number_of_labels(available_space, true);
+    println!("DEBUG: preferred_nr_labels = {preferred_nr_labels}");
 
     let mut best_score = -0.2;
-    let mut best_result = FloatLabelSet{
+    let mut best_result = FloatLabelSet {
         nr_labels: 0,
-        labels: [0.0; MAX_LABELS]
+        labels: [0.0; MAX_LABELS],
     };
 
     let mut q: f64;
     let mut step_size: f64;
-    for exponent in [base_exponent, base_exponent-1] {
+    for exponent in [base_exponent, base_exponent - 1] {
         // Find closest "zero" and thus the start of the label generation
         let f = x_min / 10_f64.powf(exponent as f64 + 1.0);
         let label_start = f.floor() * 10_f64.powf(exponent as f64 + 1.0);
@@ -38,15 +40,21 @@ fn float_axis_labels(x_min: f64, x_max: f64, _available_space: i64) -> String {
         for j in 1..(MAX_SKIP_AMOUNT + 1) {
             // i is the index of the currently selected "nice" number q for i, q in
             // enumerate(Q_VALUES):
-            for i in 0..(Q_VALUES.len()) {
-                q = Q_VALUES[i];
+            for ix in 0..(Q_VALUES.len()) {
+                let i = ix as i32;
+                q = Q_VALUES[ix];
                 step_size = q * (j as f64) * 10_f64.powf(exponent as f64);
-                let labels = linspace(label_start, x_max, step_size);
+                let labels = linspace(label_start, x_min, x_max, step_size);
                 if labels.len() < 2 {
                     // A single label is not meaningful
                     continue;
                 }
                 println!("DEBUG: Checking labels {:?} ...", labels);
+
+                let simplicity = compute_simplicity_score(&labels, i, j);
+                let coverage = compute_coverage_score(&labels, x_min, x_max);
+                let density = compute_density_score(&labels, preferred_nr_labels);
+                println!("-> simplicity = {simplicity}, coverage = {coverage}, density = {density}");
             }
         }
     }
@@ -54,13 +62,52 @@ fn float_axis_labels(x_min: f64, x_max: f64, _available_space: i64) -> String {
     return String::from("   ");
 }
 
-fn linspace(start: f64, stop: f64, step: f64) -> Vec<f64> {
+fn linspace(zero_point: f64, min: f64, max: f64, step: f64) -> Vec<f64> {
     let mut vec: Vec<f64> = Vec::new();
-    let mut x = start;
-    while x < stop {
-        vec.push(x);
+    let mut x = zero_point;
+
+    while x < max {
+        if x >= min {
+            // This accumulates numerical error, but should be fine for now.
+            vec.push(x);
+        }
         x += step;
     }
 
     return vec;
+}
+
+/// Compute an estimate for the preferred number of labels.
+fn compute_preferred_number_of_labels(available_space: i64, vertical_direction: bool) -> i32 {
+    let best_spacing = if vertical_direction { 5.6 } else { 15.0 };
+    let preferred_nr_labels = ((available_space as f32) / best_spacing) as i32;
+
+    return std::cmp::max(2, std::cmp::min(20, preferred_nr_labels));
+}
+
+/// Simplicity score according to Talbot.
+fn compute_simplicity_score(_labels: &Vec<f64>, i: i32, j: i32) -> f64 {
+    // Indicator variable that is one if zero is part of the labels, and zero otherwise
+    // NOTE It might make sense to extend this to all gridline values, plus zero
+    let v = 0.0; // TODO (any(np.isclose(labels, np.zeros(len(labels)))) as usize);
+    return 1.0 - ((i as f64) - 1.0) / ((Q_VALUES.len() as f64) - 1.0) - (j as f64) + v;
+}
+
+/// Coverage score according to Talbot.
+fn compute_coverage_score(labels: &Vec<f64>, x_min: f64, x_max: f64) -> f64 {
+    if labels.len() < 2 {
+        return 0.0;
+    }
+    return 1.0
+        - 5.0
+            * ((x_max - labels.last().unwrap()).powf(2.0)
+                + (x_min - labels.first().unwrap()).powf(2.0))
+            / ((x_max - x_min).powf(2.0));
+}
+
+/// Density score according to Talbot.
+fn compute_density_score(labels: &Vec<f64>, preferred_nr: i32) -> f64 {
+    let n = labels.len() as f64;
+    let p = preferred_nr as f64;
+    return 1.0 - f64::max(n / p, p / n);
 }
