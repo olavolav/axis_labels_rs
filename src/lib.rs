dd8f2a1a1f26094cf70ab7695bf2334cfc224316
Olav Stetter <olav.stetter@googlemail.com>
2024-11-17 10:19:00 +0100

    🎨 Minor: Code style.

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,6 @@ pub fn float_axis_labels(
 
     let mut best_score = -2.0;
     let mut best_result = String::new();
-    let mut q: f64;
     let mut step_size: f64;
     for exponent in [base_exponent, base_exponent - 1] {
         // Find closest "zero" and thus the start of the label generation
@@ -37,10 +36,9 @@ pub fn float_axis_labels(
 
         // j is the "skip amount"
         for j in 1..(MAX_SKIP_AMOUNT + 1) {
-            // i is the index of the currently selected "nice" number q
-            for ix in 0..(Q_VALUES.len()) {
+            // ix (or later i) is the index of the currently selected "nice" number q
+            for (ix, q) in Q_VALUES.iter().enumerate() {
                 let i = ix as i32;
-                q = Q_VALUES[ix];
                 step_size = q * (j as f64) * 10_f64.powf(exponent as f64);
                 let labels = crate::utils::linspace(label_start, x_min, x_max, step_size);
                 if labels.len() < 2 {
@@ -48,18 +46,22 @@ pub fn float_axis_labels(
                     continue;
                 }
 
-                let simplicity =
+                let simplicity_score =
                     crate::scoring::compute_simplicity_score(&labels, i, j, Q_VALUES.len());
-                let coverage = crate::scoring::compute_coverage_score(&labels, x_min, x_max);
-                let density = crate::scoring::compute_density_score(&labels, preferred_nr_labels);
-                let score_upper_bound =
-                    crate::scoring::upper_bound_on_overall_score(simplicity, coverage, density);
+                let coverage_score = crate::scoring::compute_coverage_score(&labels, x_min, x_max);
+                let density_score =
+                    crate::scoring::compute_density_score(&labels, preferred_nr_labels);
+                let score_upper_bound = crate::scoring::upper_bound_on_overall_score(
+                    simplicity_score,
+                    coverage_score,
+                    density_score,
+                );
                 if (!best_result.is_empty()) && (score_upper_bound < best_score) {
                     continue;
                 }
 
-                // We may have found a new best label set, depending on the last score, which is
-                // `grid_alignment`.
+                // We may have found a new best label set, depending on the last entry in the score
+                // vector, which is `grid_alignment`.
                 let (result, grid_overlap) = crate::rendering::render(
                     &labels,
                     x_min,
@@ -69,10 +71,14 @@ pub fn float_axis_labels(
                     vertical_direction,
                     &unit,
                 );
-                // TODO Full alignment score incliding regularity
-                let grid_alignment = 1.0 - ((grid_overlap as i32) as f64);
-                let score =
-                    crate::scoring::overall_score(simplicity, coverage, density, grid_alignment);
+                // TODO Full alignment score including regularity
+                let grid_alignment_score = 1.0 - ((grid_overlap as i32) as f64);
+                let score = crate::scoring::overall_score(
+                    simplicity_score,
+                    coverage_score,
+                    density_score,
+                    grid_alignment_score,
+                );
                 if score > best_score {
                     best_score = score;
                     best_result = result;
